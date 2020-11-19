2e1af0c9f441f8d8b8b6b853b39c7191b6374bb1
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-11-19 13:55:11 -0300

    creates trainer schema tests

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ex_mon_api, ExMonApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "jhonatanmartins",
+  password: "",
   database: "ex_mon_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
